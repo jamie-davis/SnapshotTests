048af41f60f3cecfe5c7e9b4ed45c06ca622f772
Jamie Davis <jamie@clandavis.net>
2020-11-22 10:11:57 +0000

    Added an extra test.

--- src/SnapshotTests.Tests/Snapshots/TestDifferenceEnhancer.cs
+++ src/SnapshotTests.Tests/Snapshots/TestDifferenceEnhancer.cs
@@ -100,6 +100,42 @@ namespace SnapshotTests.Tests.Snapshots
         }
 
         [Test]
+        public void MissingReferencesAreIgnored()
+        {
+            //Arrange
+            var table1 = _om.Make<SnapshotObjectMother.Table>(1, 2, 3, 4, 5);
+            var table2 = _om.Make<SnapshotObjectMother.Table2>(1, 2, 3, 4, 5);
+            var refTable1 = _om.Make<SnapshotObjectMother.ReferencingTable>((1, 1),(2, 1));
+
+            void TakeSnapshot(string name)
+            {
+                var builder = _om.NewSnapshot(name);
+                table1.ToSnapshotTable(builder);
+                table2.ToSnapshotTable(builder);
+                refTable1.ToSnapshotTable(builder);
+            }
+
+            TakeSnapshot("Before");
+            refTable1[0].Variable = "edited";
+            refTable1[1].ParentId = 3; //will required table1 row with key of 3 and also 2 because that is the before
+            TakeSnapshot("After");
+
+            var before = _om.GetSnapshot("Before");
+            var after = _om.GetSnapshot("After");
+
+            var diffs = SnapshotDifferenceCalculator.GetDifferences(_om.Collection, before, after);
+            var rows = ReferencedRowLocator.GetMissingRows(_om.Collection, diffs);
+
+            //Act
+            var result = DifferenceEnhancer.RequireRows(_om.Collection, diffs, rows, before);
+
+            //Assert
+            var output = new Output();
+            result.Report(output);
+            output.Report.Verify();
+        }
+
+        [Test]
         public void TablesWithNoReferencesArePreserved()
         {
             //Arrange
